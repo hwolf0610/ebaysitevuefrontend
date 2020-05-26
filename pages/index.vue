aa050573b9784d5911e6ebec5626bc24178608af
hwolf0610 <hwolf0610@hotmail.com>
2020-05-26 20:22:21 +0800

    add vue link function

--- pages/index.vue
+++ pages/index.vue
@@ -2,47 +2,36 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        ebaySiteVue
-      </h1>
-      <h2 class="subtitle">
-        My grand Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="/login"
-          target="_blank"
-          class="button--green"
-        >
-          Login
-        </a>
-        <a
-          href="/signup"
-          target="_blank"
-          class="button--grey"
-        >
-          Signup
-        </a>
-        <a
-          href="/packageplan"
-          target="_blank"
-          class="button--grey"
-        >
-          Package Plan
-        </a>
-      </div>
+      <h1 class="title">ebaySiteVue</h1>
+      <h2 class="subtitle">My grand Nuxt.js project</h2>
+
+      <nuxt-link to="/login" class="button--green">
+        <span>Login</span>
+      </nuxt-link>
+      <nuxt-link to="/signup" class="button--green">
+        <span>Signup</span>
+      </nuxt-link>
+      <nuxt-link to="/packageplan" class="button--green">
+        <span>Package Plan</span>
+      </nuxt-link>
+
+      <!-- <div class="links">      
+        <a href="/login" target="_blank" class="button--green">Login</a>
+        <a href="/signup" target="_blank" class="button--grey">Signup</a>
+        <a href="/packageplan" target="_blank" class="button--grey">Package Plan</a>
+      </div>-->
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>
@@ -56,8 +45,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
